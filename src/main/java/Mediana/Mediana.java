7f14c91c1e00bcbf72b34bac24ac1b3497d6d2fd
Bruno Daguano <bruno_daguano@hotmail.com>
2018-08-24 16:46:02 -0300

    Refatoração

--- src/main/java/Mediana/Mediana.java
+++ src/main/java/Mediana/Mediana.java
@@ -2,21 +2,24 @@ package Mediana;
 
 public class Mediana {
 
+    private double mediana = 0;
+    private int aux = 0;
+
     public double getMediana(double[] v) {
 
-        double mediana = 0;
-        int aux = 0;
-        
         for (int i = 0; i < v.length; i++) {
             aux++;
         }
-        
+
         if (aux % 2 == 1) {
-            mediana = v [v.length/2];
-        }else{
+
+            mediana = v[v.length / 2];
+
+        } else {
+
             aux = aux / 2;
-            mediana = v[aux] + v[aux-1];
-            
+            mediana = v[aux] + v[aux - 1];
+
             mediana = mediana / 2;
         }
 
